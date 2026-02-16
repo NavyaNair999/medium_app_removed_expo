f1ae8a6a45d0c3497cf962922d6bfed8e5617556
Avinash Khodke <operatorshera@gmail.com>
2026-02-16 19:12:38 +0530

    Restore React Native CLI architecture, fix navigation, align versions, fix vector icons

--- types/navigation.ts
+++ types/navigation.ts
@@ -1,15 +1,15 @@
-import type { NavigatorScreenParams } from '@react-navigation/native';
+import type { ParamListBase } from '@react-navigation/native';
 
-export type TabParamList = {
+export interface RootStackParamList extends ParamListBase {
+  Tabs: undefined;
+  Article: { id: string };
+  Settings: undefined;
+}
+
+export interface TabParamList extends ParamListBase {
   Home: undefined;
   Following: undefined;
   Search: undefined;
   Library: undefined;
   Profile: undefined;
-};
-
-export type RootStackParamList = {
-  Tabs: NavigatorScreenParams<TabParamList>;
-  Article: { id?: string } | undefined;
-  Settings: undefined;
-};
+}
